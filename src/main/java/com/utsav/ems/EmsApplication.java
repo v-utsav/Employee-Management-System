26a04bf40e79ae6ee3dfe861dab3cadcb48ab38e
v-utsav <79682229+v-utsav@users.noreply.github.com>
2024-08-11 09:00:35 +0530

    Add files via upload

---
+++ src/main/java/com/utsav/ems/EmsApplication.java
@@ -0,0 +1,14 @@
+package com.utsav.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+		System.out.println("Application Running!");
+	}
+
+}
